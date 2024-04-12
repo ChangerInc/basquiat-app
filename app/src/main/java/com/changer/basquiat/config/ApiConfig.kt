47e967854d29c72d67f9d8ca606583c97cc9a58b
MateusjfSilva <mateus.jsilva@sptech.school>
2024-04-12 15:36:04 -0300

    Adicionado objeto ApiConfig

---
+++ app/src/main/java/com/changer/basquiat/config/ApiConfig.kt
@@ -0,0 +1,18 @@
+package com.changer.basquiat.config
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiConfig {
+    const val BASE_URL = "http://localhost:8080"
+
+    fun getIntance(): Retrofit {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .build()
+    }
+}
